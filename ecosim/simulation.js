299c3324c2e5f59fc3dbdda91f89dec69c23a1ad
CarlosVazPI <carlos.vazpi@gmail.com>
2020-02-22 10:35:28 +0000

    Added sub readme

--- ecosim/simulation.js
+++ ecosim/simulation.js
@@ -1,10 +1,13 @@
 const { getId } = require('./util')
 
-const Simulation = module.exports.Simulation = function Simulation () {
+const Simulation = module.exports.Simulation = function Simulation (wealthTax, wealthTaxCycle, valueAddedTax) {
 	this.id = getId()
 	this.market = []
 	this.agents = []
 	this.state
+	this.wealthTax = wealthTax
+	this.wealthTaxCycle = wealthTaxCycle
+	this.valueAddedTax = valueAddedTax
 }
 
 Simulation.states = {}
